bd80c71093f1c50704dfbf6a08c257ab2da09ae1
Miran-Isam <meeranesam@gmail.com>
2021-03-15 14:12:14 +0200

    hh

---
+++ app/src/main/java/com/example/myeapplication/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.myeapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText ed1 = findViewById(R.id.weigh1);
+    EditText ed2 = findViewById(R.id.heigh1);
+
+    TextView tex1 =findViewById(R.id.textView2);
+    Button b1 = (Button)findViewById(R.id.btm);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result();
+            }
+        });
+
+    }
+
+    private void resultt() {
+        float a =Float.parseFloat(ed1.getText().toString())/100;
+        float b =Float.parseFloat(ed2.getText().toString())/100;
+        float c= b/(a*a);
+        tex1.setText(""+c);
+    }
+
+}
